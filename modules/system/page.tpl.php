9b0bd363c12fae65973c84c55b43997c4558a8fc
ben-agaric <ben@agaricdesign.com>
2009-05-13 07:01:19 -0400

    Updated to lates Drupal head via CVS.

--- modules/system/page.tpl.php
+++ modules/system/page.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: page.tpl.php,v 1.18 2009/03/02 19:23:54 webchick Exp $
+// $Id: page.tpl.php,v 1.20 2009/05/12 13:43:44 dries Exp $
 
 /**
  * @file
@@ -41,8 +41,6 @@
  *   in theme settings.
  * - $site_slogan: The slogan of the site, empty when display has been disabled
  *   in theme settings.
- * - $mission: The text of the site mission, empty when display has been disabled
- *   in theme settings.
  *
  * Navigation:
  * - $search_box: HTML to display the search box, empty if search has been disabled.
@@ -62,9 +60,9 @@
  * - $feed_icons: A string of all feed icons for the current page.
  * - $left: The HTML for the left sidebar.
  * - $right: The HTML for the right sidebar.
+ * - $highlight: The HTML for the highlighted content region.
  *
  * Footer/closing data:
- * - $footer_message: The footer message as defined in the admin settings.
  * - $footer : The footer region.
  * - $closure: Final closing markup from any modules that have altered the page.
  *   This variable should always be output last, after all other dynamic content.
@@ -138,7 +136,7 @@
 
       <div id="content" class="column"><div class="section">
         <?php if ($breadcrumb): ?><div id="breadcrumb"><?php print $breadcrumb; ?></div><?php endif; ?>
-        <?php if ($mission): ?><div id="mission"><?php print $mission; ?></div><?php endif; ?>
+        <?php if ($highlight): ?><div id="highlight"><?php print $highlight; ?></div><?php endif; ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print $messages; ?>
         <?php if ($tabs): ?><div class="tabs"><?php print $tabs; ?></div><?php endif; ?>
@@ -165,7 +163,6 @@
 
     <div id="footer"><div class="section">
       <?php print theme('links', $secondary_menu, array('id' => 'secondary-menu', 'class' => 'links clearfix')); ?>
-      <?php if ($footer_message): ?><div id="footer-message"><?php print $footer_message; ?></div><?php endif; ?>
       <?php if ($footer): ?><div id="footer-region" class="region"><?php print $footer; ?></div><?php endif; ?>
     </div></div> <!-- /.section, /#footer -->
 
